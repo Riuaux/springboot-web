aebb2b7c47ceb94b08616377b3c7a9f40fe96d89
Riuaux <agustin.fullstack@gmail.com>
2025-03-09 01:19:50 -0600

    MOdify User Model, list all, conditional render

--- src/main/java/com/riuaux/spring/learning/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/riuaux/spring/learning/webapp/springboot_web/controllers/UserController.java
@@ -1,13 +1,26 @@
 package com.riuaux.spring.learning.webapp.springboot_web.controllers;
 
+// import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.riuaux.spring.learning.webapp.springboot_web.models.UserModel;
 
 @Controller
 public class UserController {
+
+  @GetMapping("/users")
+  public String users(ModelMap model) {
+    model.addAttribute("pageTitle", "Conditional display");
+
+    return "users";
+  }
 
   @GetMapping("/details")
   public String details(Model model) {
@@ -17,5 +30,13 @@ public class UserController {
     model.addAttribute("pageTitle", "Testing Spring");
 
     return "details";
+  }
+
+  @ModelAttribute("users")
+  public List<UserModel> someUsers() {
+    return Arrays.asList(
+        new UserModel("User", "One", "email@example.rnd", "asdf1234"),
+        new UserModel("User", "Two"));
+
   }
 }
--- src/main/java/com/riuaux/spring/learning/webapp/springboot_web/models/UserModel.java
+++ src/main/java/com/riuaux/spring/learning/webapp/springboot_web/models/UserModel.java
@@ -4,6 +4,17 @@ public class UserModel {
 
   private String firstName;
   private String lastName;
+  private String email;
+  private String password;
+
+  public UserModel() {
+  }
+
+  public UserModel(String firstName, String lastName, String email, String password) {
+    this(firstName, lastName);
+    this.email = email;
+    this.password = password;
+  }
 
   public UserModel(String firstName, String lastName) {
     this.firstName = firstName;
@@ -24,6 +35,22 @@ public class UserModel {
 
   public void setLastName(String lastName) {
     this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
   }
 
 }
