e3318c26c365b9b27aa4e91d302c2cd9c7186215
Riuaux <agustin.fullstack@gmail.com>
2025-03-08 23:42:12 -0600

    Model's attrs and props for controller/view

--- src/main/java/com/riuaux/spring/learning/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/riuaux/spring/learning/webapp/springboot_web/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.riuaux.spring.learning.webapp.springboot_web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.web.bind.annotation.RequestParam;
 
@@ -8,7 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UserController {
 
   @GetMapping("/details")
-  public String details(/* @RequestParam String param */) {
+  public String details(Model model) {
+    model.addAttribute("pageTitle", "Testing Spring");
+    model.addAttribute("firstName", "Agustin");
+    model.addAttribute("lastName", "Camarena");
+
     return "details";
   }
 }
