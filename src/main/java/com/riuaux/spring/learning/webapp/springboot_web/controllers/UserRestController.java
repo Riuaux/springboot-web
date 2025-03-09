1a4b691e3aa00848cfc955f1727b215d33894d50
Riuaux <agustin.fullstack@gmail.com>
2025-03-09 00:38:13 -0600

    List of Users

--- src/main/java/com/riuaux/spring/learning/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/riuaux/spring/learning/webapp/springboot_web/controllers/UserRestController.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.riuaux.spring.learning.webapp.springboot_web.models.UserDTO;
 import com.riuaux.spring.learning.webapp.springboot_web.models.UserModel;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
+
+  @GetMapping("/users")
+  public List<UserModel> users() {
+    UserModel user1 = new UserModel("User", "One");
+    UserModel user2 = new UserModel("User", "Two");
+    UserModel user3 = new UserModel("User", "Three");
+
+    List<UserModel> users = Arrays.asList(user1, user2, user3);
+
+    return users;
+  }
 
   @GetMapping("/details")
   public Map<String, Object> userDetails() {
