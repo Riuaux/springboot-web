9a2f2cf920afc5ea4dc94350f9215f8034136106
Riuaux <agustin.fullstack@gmail.com>
2025-03-09 00:10:22 -0600

    Apply Model for User

--- src/main/java/com/riuaux/spring/learning/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/riuaux/spring/learning/webapp/springboot_web/controllers/UserController.java
@@ -3,16 +3,18 @@ package com.riuaux.spring.learning.webapp.springboot_web.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RequestParam;
+
+import com.riuaux.spring.learning.webapp.springboot_web.models.UserModel;
 
 @Controller
 public class UserController {
 
   @GetMapping("/details")
   public String details(Model model) {
+    UserModel user = new UserModel("Agustin", "Camarena");
+    model.addAttribute("user", user);
+
     model.addAttribute("pageTitle", "Testing Spring");
-    model.addAttribute("firstName", "Agustin");
-    model.addAttribute("lastName", "Camarena");
 
     return "details";
   }
--- src/main/java/com/riuaux/spring/learning/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/riuaux/spring/learning/webapp/springboot_web/controllers/UserRestController.java
@@ -2,6 +2,8 @@ package com.riuaux.spring.learning.webapp.springboot_web.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.riuaux.spring.learning.webapp.springboot_web.models.UserModel;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,10 +16,10 @@ public class UserRestController {
 
   @GetMapping("/details")
   public Map<String, Object> getJSON() {
+    UserModel user = new UserModel("Agustin", "Camarena");
     Map<String, Object> responseBody = new HashMap<>();
-    responseBody.put("pageTitle", "Testing Spring");
-    responseBody.put("firstName", "Agustin");
-    responseBody.put("lastName", "Camarena");
+
+    responseBody.put("user", user);
 
     return responseBody;
   }
