064292582bd74ea6883e7a7a2541dfacbe3e2556
Riuaux <agustin.fullstack@gmail.com>
2025-03-12 18:31:44 -0600

    Injection of .properties files

--- src/main/java/com/riuaux/spring/learning/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/riuaux/spring/learning/webapp/springboot_web/controllers/PathVariableController.java
@@ -9,6 +9,7 @@ import com.riuaux.spring.learning.webapp.springboot_web.models.dtos.ParamDto;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("/api")
 public class PathVariableController {
+
+  // ? Getting constants from a .properties file, "injection"
+  @Value("${config.username}")
+  private String username;
+
+  // @Value("${config.message}")
+  // private String message;
+
+  @Value("${config.code}")
+  private Integer code;
+
+  @Value("${config.listValues}")
+  private String[] listValues;
 
   @GetMapping("/variable/{msg}")
   /**
@@ -52,6 +66,22 @@ public class PathVariableController {
     user.setPassword(user.getPassword().toUpperCase());
 
     return user;
+  }
+
+  // ^ Working with Value constants declared at top
+
+  @GetMapping("/values")
+  public Map<String, Object> values(
+      // ? Passed as arg instead of injection
+      @Value("${config.message}") String message) {
+
+    Map<String, Object> jsonData = new HashMap<>();
+    jsonData.put("username", username);
+    jsonData.put("message", message);
+    jsonData.put("code", code);
+    jsonData.put("listValues", listValues);
+
+    return jsonData;
   }
 
 }
